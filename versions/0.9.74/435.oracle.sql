1cf9193b1d83c7687af7370f706e7b41744d9f9a
Josep Gayà <josepg@limit.es>
2020-06-08 10:43:56 +0200

    #435: Error al crear flux de firmes

---
+++ versions/0.9.74/435.oracle.sql
@@ -0,0 +1,254 @@
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD01',
+    'Resolución',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD02',
+    'Acuerdo',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD03',
+    'Contrato',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD04',
+    'Convenio',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD05',
+    'Declaración',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD06',
+    'Comunicación',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD07',
+    'Notificación',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD08',
+    'Publicación',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD09',
+    'Acuse de recibo',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD10',
+    'Acta',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD11',
+    'Certificado',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD12',
+    'Diligencia',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD13',
+    'Informe',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD14',
+    'Solicitud',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD15',
+    'Denuncia',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD16',
+    'Alegación',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD17',
+    'Recurso',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD18',
+    'Comunicación ciudadano',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD19',
+    'Factura',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD20',
+    'Otros incautados',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+INSERT INTO
+    IPA_TIPUS_DOCUMENTAL(ID,CODI,NOM,ENTITAT_ID,CREATEDBY_CODI,CREATEDDATE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE)
+SELECT
+    ipa_hibernate_seq.NEXTVAL,
+    'TD99',
+    'Otros',
+    1,
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF'),
+    'sqlupdate',
+    to_timestamp(sysdate,'DD/MM/RR HH24:MI:SSXFF')
+FROM dual;
+
+
