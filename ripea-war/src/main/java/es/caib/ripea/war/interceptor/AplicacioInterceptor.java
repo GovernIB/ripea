50d68e1760e9745573bdd26cc0a601b608a0ef2a
Josep Gayà <josepg@limit.es>
2020-09-15 10:52:29 +0200

    #525 Modificar el peu de pàgina per a mostrar la versió i la data de compilació

--- ripea-war/src/main/java/es/caib/ripea/war/interceptor/AplicacioInterceptor.java
+++ ripea-war/src/main/java/es/caib/ripea/war/interceptor/AplicacioInterceptor.java
@@ -3,8 +3,14 @@
  */
 package es.caib.ripea.war.interceptor;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
@@ -16,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.support.RequestContextUtils;
+
 
 /**
  * Interceptor per a les accions de context d'aplicació.
@@ -44,7 +51,14 @@ public class AplicacioInterceptor extends HandlerInterceptorAdapter {
 			for (Object key: new HashMap(manifestAtributs).keySet()) {
 				manifestAtributsMap.put(key.toString(), manifestAtributs.get(key));
 			}
+			Locale locale = new Locale(RequestContextUtils.getLocale(request).getLanguage(), 
+					RequestContextUtils.getLocale(request).getCountry());
+			manifestAtributsMap.put(
+					"Build-Timestamp",
+					formatBuildTimestamp(
+							manifestAtributsMap.get("Build-Timestamp").toString(), locale));
 		}
+
 		request.setAttribute(
 				REQUEST_ATTRIBUTE_MANIFEST_ATRIBUTES,
 				manifestAtributsMap);
@@ -52,6 +66,16 @@ public class AplicacioInterceptor extends HandlerInterceptorAdapter {
 				"requestLocale",
 				RequestContextUtils.getLocale(request).getLanguage());
 		return true;
+	}
+	
+	private String formatBuildTimestamp(String timestamp, Locale locale) throws ParseException {
+		String ISO_DATE_FORMAT_ZERO_OFFSET = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(ISO_DATE_FORMAT_ZERO_OFFSET);
+		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY.MM.dd HH:mm", locale);		
+		Date date = simpleDateFormat.parse(timestamp);
+		return dateFormat.format(date) + "h";
 	}
 
 }
--- ripea-war/src/main/webapp/WEB-INF/tags/ripea/versio.tag
+++ ripea-war/src/main/webapp/WEB-INF/tags/ripea/versio.tag
@@ -1,4 +1,4 @@
 ${manifestAtributes['Implementation-Version']}
 <span id="versioData" style="color:rgba(0,0,0,0);">
-	${manifestAtributes['Implementation-SCM-Branch']}, ${manifestAtributes['Implementation-SCM-Revision']}, ${manifestAtributes['Build-Timestamp']}
+	(${manifestAtributes['Build-Timestamp']})
 </span>
