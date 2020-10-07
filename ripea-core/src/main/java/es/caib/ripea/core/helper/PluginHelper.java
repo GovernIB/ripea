b3b36dd80ce230bcdb51f41afe80423f37ac1089
josepg-limit <josepg@limit.es>
2020-10-07 14:23:36 +0200

    #580: Problema amb Núm Expedient al notificar

--- ripea-core/src/main/java/es/caib/ripea/core/helper/PluginHelper.java
+++ ripea-core/src/main/java/es/caib/ripea/core/helper/PluginHelper.java
@@ -182,6 +182,8 @@ public class PluginHelper {
 	private AplicacioService aplicacioService;
 	@Autowired
 	private UnitatOrganitzativaHelper unitatOrganitzativaHelper;
+	@Autowired
+	private ExpedientHelper expedientHelper;
 
 	public List<String> rolsUsuariFindAmbCodi(
 			String usuariCodi) {
@@ -3220,7 +3222,7 @@ public class PluginHelper {
 			}
 			
 			notificacio.setProcedimentCodi(metaExpedient.getClassificacioSia());
-			notificacio.setNumExpedient(expedientEntity.getCodi());
+			notificacio.setNumExpedient(expedientHelper.calcularNumero(expedientEntity));
 			
 			UsuariDto usuari = aplicacioService.getUsuariActual();
 			List<Enviament> enviaments = new ArrayList<>();
