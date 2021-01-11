82c185f1d65bfd3c742dd9e72846e05e10182c75
Bernat Galmés <josepg@limit.es>
2021-01-11 13:39:34 +0100

    #639 No s'actualiten les dades estadístiques dels documents

--- ripea-core/src/main/java/es/caib/ripea/core/task/HistoricTask.java
+++ ripea-core/src/main/java/es/caib/ripea/core/task/HistoricTask.java
@@ -6,11 +6,13 @@ import java.util.Date;
 import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.caib.ripea.core.api.dto.historic.HistoricTipusEnumDto;
 import es.caib.ripea.core.helper.HistoricHelper;
 
+@Component
 public class HistoricTask {
 
 	@Autowired
