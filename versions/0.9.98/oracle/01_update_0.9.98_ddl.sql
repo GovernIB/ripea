8be3b84db91dc074dcf0d06519dfa206e42f794f
Josep Gayà <josepg@limit.es>
2023-03-20 10:35:02 +0100

    Preparar versió 0.9.98

---
+++ versions/0.9.98/oracle/01_update_0.9.98_ddl.sql
@@ -0,0 +1,6 @@
+-- Changeset db/changelog/changes/0.9.98/1186.yaml::1677771425170-1::limit
+ALTER TABLE ipa_document ADD expedient_estat_id NUMBER(19);
+
+ALTER TABLE ipa_document ADD CONSTRAINT ipa_expestat_document_fk FOREIGN KEY (expedient_estat_id) REFERENCES ipa_expedient_estat (id);
+
+ALTER TABLE ipa_usuari ADD vista_actual VARCHAR2(64 CHAR);
