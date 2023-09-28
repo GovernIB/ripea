3cd9c776ee0d768f0596ab13e083aa253615b78c
Josep Gayà <josepg@limit.es>
2023-09-28 10:06:00 +0200

    Preparar versió 0.9.102

---
+++ versions/0.9.102/postgresql/02_update_0.9.102_dml.sql
@@ -0,0 +1,10 @@
+-- Changeset db/changelog/changes/0.9.102/1317.yaml::1695202086434-1::limit
+update ipa_massiva_contingut set element_id = contingut_id;
+
+update ipa_massiva_contingut set element_tipus = 'DOCUMENT';
+
+update ipa_massiva_contingut set element_nom = (select nom from ipa_contingut where ipa_contingut.id = ipa_massiva_contingut.contingut_id);
+
+-- Changeset db/changelog/changes/0.9.102/954.yaml::1694769935247-1::limit
+INSERT INTO ipa_config (key, value, description, group_code, position, jboss_property, type_code, configurable) VALUES ('es.caib.ripea.entorn', NULL, 'Entorn on es troba l''aplicació', 'GENERAL', '4', '0', 'TEXT', '0');
+
