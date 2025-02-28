ff7825596ad07b80bd2f772582c20abe64fe9b05
josepg-limit <josepg@limit.es>
2025-02-28 16:04:20 +0100

    Correccions de errors de unnamed module i altre modificacions menors.
    - Pendent resoldre error:
    NoSuchMethodException-->com.turkraft.springfilter.boot.SpringFilterAutoConfig$$EnhancerBySpringCGLIB$$1d5a1c4a.CGLIB$SET_THREAD_CALLBACKS

---
+++ ripea-api-interna/src/main/java/es/caib/ripea/api/interna/config/EjbClientApiIntConfig.java
@@ -0,0 +1,49 @@
+package es.caib.ripea.api.interna.config;
+
+import es.caib.ripea.service.intf.config.BaseConfig;
+import es.caib.ripea.service.intf.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWarDeployment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ejb.access.LocalStatelessSessionProxyFactoryBean;
+
+/**
+ * Configuració d'accés als services de Spring mitjançant EJBs.
+ *
+ * @author Límit Tecnologies
+ */
+@Slf4j
+@Configuration
+public class EjbClientApiIntConfig {
+
+	static final String EJB_JNDI_PREFIX = "java:app/" + BaseConfig.APP_NAME + "-ejb/";
+	static final String EJB_JNDI_SUFFIX = "Ejb";
+
+	@Bean
+	@ConditionalOnWarDeployment
+	public LocalStatelessSessionProxyFactoryBean documentService() {
+		return getLocalEjbFactoyBean(DocumentService.class);
+	}
+	
+	@Bean
+	@ConditionalOnWarDeployment
+	public LocalStatelessSessionProxyFactoryBean historicService() {
+		return getLocalEjbFactoyBean(HistoricService.class);
+	}
+	
+	private LocalStatelessSessionProxyFactoryBean getLocalEjbFactoyBean(Class<?> serviceClass) {
+		String jndiName = jndiServiceName(serviceClass, false);
+		log.info("Creating EJB proxy for " + serviceClass.getSimpleName() + " with JNDI name " + jndiName);
+		LocalStatelessSessionProxyFactoryBean factoryBean = new LocalStatelessSessionProxyFactoryBean();
+		factoryBean.setBusinessInterface(serviceClass);
+		factoryBean.setExpectedType(serviceClass);
+		factoryBean.setJndiName(jndiName);
+		return factoryBean;
+	}
+
+	private String jndiServiceName(Class<?> serviceClass, boolean addServiceClassName) {
+		return EJB_JNDI_PREFIX + serviceClass.getSimpleName() + EJB_JNDI_SUFFIX + (addServiceClassName ? "!" + serviceClass.getName() : "");
+	}
+
+}
