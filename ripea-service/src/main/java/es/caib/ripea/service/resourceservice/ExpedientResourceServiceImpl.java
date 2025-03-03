c91ebda6e02fa436a3f1c445dda602a982469bc5
Josep Gayà <josepg@limit.es>
2025-03-03 12:31:52 +0100

    888 - Actualització estàndards RIPEA

--- ripea-service/src/main/java/es/caib/ripea/service/resourceservice/ExpedientResourceServiceImpl.java
+++ ripea-service/src/main/java/es/caib/ripea/service/resourceservice/ExpedientResourceServiceImpl.java
@@ -3,6 +3,8 @@ package es.caib.ripea.service.resourceservice;
 import es.caib.ripea.persistence.entity.resourceentity.ExpedientResourceEntity;
 import es.caib.ripea.persistence.entity.resourcerepository.InteressatResourceRepository;
 import es.caib.ripea.service.base.service.BaseMutableResourceService;
+import es.caib.ripea.service.intf.base.exception.AnswerRequiredException;
+import es.caib.ripea.service.intf.base.exception.PerspectiveApplicationException;
 import es.caib.ripea.service.intf.model.ExpedientResource;
 import es.caib.ripea.service.intf.model.InteressatResource;
 import es.caib.ripea.service.intf.resourceservice.ExpedientResourceService;
@@ -11,8 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -25,41 +27,55 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ExpedientResourceServiceImpl extends BaseMutableResourceService<ExpedientResource, Long, ExpedientResourceEntity> implements ExpedientResourceService {
 
-    private final InteressatResourceRepository interessatResourceRepository;
+	private final InteressatResourceRepository interessatResourceRepository;
 
-    @PostConstruct
+	@PostConstruct
 	public void init() {
+		register("INTERESSATS_RESUM", new InteressatsPerspectiveApplicator());
+		register("metaExpedient", new MetaExpedientOnchangeLogicProcessor());
 	}
 
-    @Override
-    protected ExpedientResource applyPerspectives(ExpedientResourceEntity entity, ExpedientResource resource, String[] perspectives) {
-        if(Arrays.asList(perspectives).contains("INTERESSATS_RESUM")){
-            String interessatsResum = "";
-            List<InteressatResource> interessats= interessatResourceRepository.findByExpedient(entity).stream()
-                    .map(interessatResourceEntity -> objectMappingHelper.newInstanceMap(interessatResourceEntity, InteressatResource.class))
-                    .collect(Collectors.toList());
-            if (!interessats.isEmpty()) {
-                for (InteressatResource interessat : interessats) {
-                    switch (interessat.getTipus()){
-                        case InteressatPersonaFisicaEntity:
-                            interessatsResum += interessat.getNom() == null ? "" : interessat.getNom() + " ";
-                            interessatsResum += interessat.getLlinatge1() == null ? "" : interessat.getLlinatge1() + " ";
-                            interessatsResum += interessat.getLlinatge2() == null ? "" : interessat.getLlinatge2() + " ";
-                            interessatsResum += "(" + interessat.getDocumentNum() + ")" + "\n";
-                            break;
-                        case InteressatPersonaJuridicaEntity:
-                            interessatsResum += interessat.getRaoSocial() + " ";
-                            interessatsResum += "(" + interessat.getDocumentNum() + ")" + "\n";
-                            break;
-                        case InteressatAdministracioEntity:
-                            interessatsResum += interessat.getNomComplet() + " ";
-                            interessatsResum += "(" + interessat.getDocumentNum() + ")" + "\n";
-                            break;
-                    }
-                }
-            }
-            resource.setInteressatsResum(interessatsResum);
-        }
-        return resource;
-    }
+	private class InteressatsPerspectiveApplicator implements PerspectiveApplicator<ExpedientResource, ExpedientResourceEntity> {
+		@Override
+		public void applySingle(String code, ExpedientResourceEntity entity, ExpedientResource resource) throws PerspectiveApplicationException {
+			List<InteressatResource> interessats= interessatResourceRepository.findByExpedient(entity).stream()
+					.map(interessatResourceEntity -> objectMappingHelper.newInstanceMap(interessatResourceEntity, InteressatResource.class))
+					.collect(Collectors.toList());
+			StringBuilder interessatsResum = new StringBuilder();
+			if (!interessats.isEmpty()) {
+				for (InteressatResource interessat: interessats) {
+					switch (interessat.getTipus()) {
+						case InteressatPersonaFisicaEntity:
+							interessatsResum.append(interessat.getNom() == null ? "" : interessat.getNom() + " ");
+							interessatsResum.append(interessat.getLlinatge1() == null ? "" : interessat.getLlinatge1() + " ");
+							interessatsResum.append(interessat.getLlinatge2() == null ? "" : interessat.getLlinatge2() + " ");
+							interessatsResum.append("(").append(interessat.getDocumentNum()).append(")").append("\n");
+							break;
+						case InteressatPersonaJuridicaEntity:
+							interessatsResum.append(interessat.getRaoSocial()).append(" ");
+							interessatsResum.append("(").append(interessat.getDocumentNum()).append(")").append("\n");
+							break;
+						case InteressatAdministracioEntity:
+							interessatsResum.append(interessat.getNomComplet()).append(" ");
+							interessatsResum.append("(").append(interessat.getDocumentNum()).append(")").append("\n");
+							break;
+					}
+				}
+			}
+			resource.setInteressatsResum(interessatsResum.toString());
+		}
+	}
+
+	private class MetaExpedientOnchangeLogicProcessor implements OnChangeLogicProcessor<ExpedientResource> {
+		@Override
+		public void processOnChangeLogic(
+				ExpedientResource previous,
+				String fieldName,
+				Object fieldValue,
+				Map<String, AnswerRequiredException.AnswerValue> answers,
+				ExpedientResource target) {
+
+		}
+	}
+
 }
