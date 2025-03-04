9511eddd468891cf1519b3b25f6cf7b82af81006
Josep Gayà <josepg@limit.es>
2025-03-04 14:42:44 +0100

    888 - Actualització estàndards RIPEA

--- ripea-back/src/main/jsapp/ripea-back/src/AppRoutes.tsx
+++ ripea-back/src/main/jsapp/ripea-back/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NotFoundPage from './pages/NotFound';
 import Expedient from './pages/expedient/Expedient';
 import ExpedientGrid from './pages/expedient/ExpedientGrid';
 
 const AppRoutes: React.FC = () => {
     return <Routes>
+        <Route path="/" element={<Navigate to="/expedient" />} />
         <Route path="expedient">
             <Route index element={<ExpedientGrid />} />
             {/*<Route path=":id" element={<Expedient />} />*/}
