34f2b72e683217050c7050410937ee6ccb19680c
Josep Gayà <josepg@limit.es>
2025-03-14 10:35:05 +0100

    888 - Actualització estàndards RIPEA

--- ripea-back/src/main/java/es/caib/ripea/back/base/controller/BaseReadonlyResourceController.java
+++ ripea-back/src/main/java/es/caib/ripea/back/base/controller/BaseReadonlyResourceController.java
@@ -104,7 +104,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping(value = "/{id}")
 	@Operation(summary = "Consulta la informació d'un recurs")
-	@PreAuthorize("hasPermission(#resourceId, this.getResourceClass().getName(), this.getOperation('GET_ONE'))")
+	@PreAuthorize("this.isPublic() or hasPermission(#resourceId, this.getResourceClass().getName(), this.getOperation('GET_ONE'))")
 	public ResponseEntity<EntityModel<R>> getOne(
 			@PathVariable
 			@Parameter(description = "Identificador del recurs")
@@ -130,7 +130,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping
 	@Operation(summary = "Consulta paginada de recursos")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('FIND'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('FIND'))")
 	public ResponseEntity<PagedModel<EntityModel<R>>> find(
 			@RequestParam(value = "quickFilter", required = false)
 			@Parameter(description = "Filtre ràpid (text)")
@@ -210,7 +210,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping(value = "/{id}/fields/{fieldName}/download")
 	@Operation(summary = "Descàrrega de l'arxiu associat a un camp del recurs")
-	@PreAuthorize("hasPermission(#id, this.getResourceClass().getName(), this.getOperation('FIELDDOWNLOAD'))")
+	@PreAuthorize("this.isPublic() or hasPermission(#id, this.getResourceClass().getName(), this.getOperation('FIELDDOWNLOAD'))")
 	public ResponseEntity<InputStreamResource> fieldDownload(
 			@PathVariable
 			@Parameter(description = "Identificador del recurs")
@@ -236,7 +236,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping("/artifacts")
 	@Operation(summary = "Llista d'artefactes relacionats amb aquest servei")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('ARTIFACT'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('ARTIFACT'))")
 	public ResponseEntity<CollectionModel<EntityModel<ResourceArtifact>>> artifacts() {
 		log.debug("Consulta dels artefactes disponibles pel recurs");
 		List<ResourceArtifact> artifacts = getReadonlyResourceService().artifactFindAll(null);
@@ -252,7 +252,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping("/artifacts/{type}/{code}")
 	@Operation(summary = "Informació d'un artefacte")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('ARTIFACT'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('ARTIFACT'))")
 	public ResponseEntity<EntityModel<ResourceArtifact>> artifactGetOne(
 			@PathVariable
 			@Parameter(description = "Tipus de l'artefacte")
@@ -269,7 +269,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@PostMapping("/artifacts/{type}/{code}/validate")
 	@Operation(summary = "Validació del formulari d'un artefacte")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('ARTIFACT'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('ARTIFACT'))")
 	public ResponseEntity<?> artifactValidate(
 			@PathVariable
 			@Parameter(description = "Tipus de l'artefacte")
@@ -291,7 +291,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@PostMapping("/artifacts/report/{code}")
 	@Operation(summary = "Generació de l'informe associat al recurs")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('REPORT'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('REPORT'))")
 	public ResponseEntity<CollectionModel<EntityModel<?>>> artifactReportGenerate(
 			@PathVariable
 			@Parameter(description = "Codi de l'informe")
@@ -319,7 +319,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping(value = "/artifacts/report/{code}/fields/{fieldName}/options")
 	@Operation(summary = "Consulta paginada de les opcions disponibles per a emplenar un camp de tipus ResourceReference que pertany al formulari de l'informe")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('REPORT'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('REPORT'))")
 	public <RR extends Resource<?>> ResponseEntity<PagedModel<EntityModel<RR>>> artifactReportFieldOptionsFind(
 			@PathVariable
 			@Parameter(description = "Codi de l'informe")
@@ -379,7 +379,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping(value = "/artifacts/report/{code}/fields/{fieldName}/options/{id}")
 	@Operation(summary = "Consulta d'una de les opcions disponibles per a emplenar un camp de tipus ResourceReference que pertany al formulari de l'informe")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('REPORT'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('REPORT'))")
 	public <RR extends Resource<RID>, RID extends Serializable> ResponseEntity<EntityModel<RR>> artifactReportFieldOptionsGetOne(
 			@PathVariable
 			@Parameter(description = "Codi de l'informe")
@@ -416,7 +416,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping(value = "/artifacts/filter/{code}/fields/{fieldName}/options")
 	@Operation(summary = "Consulta paginada de les opcions disponibles per a emplenar un camp de tipus ResourceReference que pertany al formulari del filtre")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('FIND'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('FIND'))")
 	public <RR extends Resource<?>> ResponseEntity<PagedModel<EntityModel<RR>>> artifactFilterFieldOptionsFind(
 			@PathVariable
 			@Parameter(description = "Codi de l'informe")
@@ -476,7 +476,7 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 	@Override
 	@GetMapping(value = "/artifacts/filter/{code}/fields/{fieldName}/options/{id}")
 	@Operation(summary = "Consulta d'una de les opcions disponibles per a emplenar un camp de tipus ResourceReference que pertany al formulari del filtre")
-	@PreAuthorize("hasPermission(null, this.getResourceClass().getName(), this.getOperation('FIND'))")
+	@PreAuthorize("this.isPublic() or hasPermission(null, this.getResourceClass().getName(), this.getOperation('FIND'))")
 	public <RR extends Resource<RID>, RID extends Serializable> ResponseEntity<EntityModel<RR>> artifactFilterFieldOptionsGetOne(
 			@PathVariable
 			@Parameter(description = "Codi de l'informe")
@@ -518,6 +518,10 @@ public abstract class BaseReadonlyResourceController<R extends Resource<? extend
 					0);
 		}
 		return resourceClass;
+	}
+
+	public boolean isPublic() {
+		return false;
 	}
 
 	public PermissionEvaluatorService.RestApiOperation getOperation(String operationName) {
