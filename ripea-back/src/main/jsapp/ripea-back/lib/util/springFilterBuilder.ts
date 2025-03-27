aec7b9706d75c75d30a7c862c63bf6db242121e2
Josep Gayà <josepg@limit.es>
2025-03-27 08:49:55 +0100

    888 - Actualització estàndards RIPEA

---
+++ ripea-back/src/main/jsapp/ripea-back/lib/util/springFilterBuilder.ts
@@ -0,0 +1,67 @@
+const filter = (options: any[]): any[] => {
+    return options.filter(a => {
+        return a != null &&
+            a.length > 0 &&
+            !a.includes('undefined') &&
+            !(a.includes('null') && !(a.includes('is not null') || a.includes('is null')));
+    });
+}
+
+export const and = (...options: any[]): string => {
+    return filter(options).join(' and ');
+}
+
+export const or = (...options: any[]): string => {
+    return filter(options).join(' or ');
+}
+
+export const like = (option: string, value: string): string => {
+    return `${option}~'%${value}%'`;
+}
+
+export const neq = (option: string, value: any): string => {
+    return value === null ? `${option} is not null` : `${option}!${value}`;
+}
+
+export const eq = (option: string, value: any): string => {
+    return value === null ? `${option} is null` : `${option}:${value}`;
+}
+
+export const equals = (option: string, value: any, equals: boolean): string => {
+    return equals ? eq(option, value) : neq(option, value);
+}
+
+export const listIn = (option: string, ...values: any[]): string => {
+    const joinedValues = filter(values).join(',')
+    return joinedValues.length > 0 ? `${option} in (${joinedValues})` : '';
+}
+
+export const concat = (...options: any[]): string => {
+    return options.length > 0 ? `concat(${filter(options).join(",' ',")})` : '';
+}
+
+export const exists = (value: string): string => {
+    return value.length > 0 ? `exists(${value})` : '';
+}
+
+export const greaterThan = (option: string, value: any): string => {
+    return `${option}>${value}`;
+}
+
+export const greaterEq = (option: string, value: any): string => {
+    return `${option}>:${value}`;
+}
+
+export const lessThan = (option: string, value: any): string => {
+    return `${option}<${value}`;
+}
+
+export const lessEq = (option: string, value: any): string => {
+    return `${option}<:${value}`;
+}
+
+export const between = (option: string, paramStart: any, paramEnd: any) => {
+    return and(
+        greaterEq(option, paramStart),
+        lessEq(option, paramEnd));
+}
