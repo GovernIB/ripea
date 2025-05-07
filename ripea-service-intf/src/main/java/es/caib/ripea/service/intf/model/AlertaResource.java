b494578fe493b51a2ac7204d1526e1f3a040edcf
Josep Gayà <josepg@limit.es>
2025-05-07 14:57:20 +0200

    #1632
    - Alertas de expediente

--- ripea-service-intf/src/main/java/es/caib/ripea/service/intf/model/AlertaResource.java
+++ ripea-service-intf/src/main/java/es/caib/ripea/service/intf/model/AlertaResource.java
@@ -1,19 +1,30 @@
 package es.caib.ripea.service.intf.model;
 
 import es.caib.ripea.service.intf.base.annotation.ResourceConfig;
+import es.caib.ripea.service.intf.base.annotation.ResourceConfigArtifact;
 import es.caib.ripea.service.intf.base.model.BaseAuditableResource;
+import es.caib.ripea.service.intf.base.model.ResourceArtifactType;
 import es.caib.ripea.service.intf.base.model.ResourceReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @Setter
 @NoArgsConstructor
-@ResourceConfig(quickFilterFields = { "text" }, descriptionField = "text")
+@ResourceConfig(
+        quickFilterFields = {"text"},
+        descriptionField = "text",
+        artifacts = {
+                @ResourceConfigArtifact(
+                        type = ResourceArtifactType.ACTION,
+                        code = AlertaResource.ACTION_MASSIVE_LLEGIT_CODE,
+                        formClass = ExpedientResource.MassiveAction.class),
+        }
+)
 public class AlertaResource extends BaseAuditableResource<Long> {
+
+    public static final String ACTION_MASSIVE_LLEGIT_CODE = "LLEGIT";
 
     private String text;
     private String error;
