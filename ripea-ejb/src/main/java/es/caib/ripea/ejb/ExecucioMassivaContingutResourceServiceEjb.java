7cf7bcd89570c1fbc28dca22aa8f31daa43febc0
josepg-limit <josepg@limit.es>
2025-05-08 16:40:57 +0200

    Preparada versió 1.0.1 per develop.

--- ripea-ejb/src/main/java/es/caib/ripea/ejb/ExecucioMassivaContingutResourceServiceEjb.java
+++ ripea-ejb/src/main/java/es/caib/ripea/ejb/ExecucioMassivaContingutResourceServiceEjb.java
@@ -1,9 +1,20 @@
 package es.caib.ripea.ejb;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 
 import es.caib.ripea.ejb.base.AbstractServiceEjb;
+import es.caib.ripea.service.intf.base.exception.ActionExecutionException;
+import es.caib.ripea.service.intf.base.exception.AnswerRequiredException;
+import es.caib.ripea.service.intf.base.exception.ArtifactNotFoundException;
+import es.caib.ripea.service.intf.base.exception.ReportGenerationException;
+import es.caib.ripea.service.intf.base.exception.ResourceFieldNotFoundException;
+import es.caib.ripea.service.intf.base.exception.AnswerRequiredException.AnswerValue;
+import es.caib.ripea.service.intf.base.model.ResourceArtifactType;
 import es.caib.ripea.service.intf.resourceservice.ExecucioMassivaContingutResourceService;
 import lombok.experimental.Delegate;
 
@@ -17,4 +28,22 @@ public class ExecucioMassivaContingutResourceServiceEjb extends AbstractServiceE
 		this.delegateService = delegateService;
 	}
 
+	@Override
+	public <P extends Serializable> Serializable artifactActionExec(Long id, String code, P params)
+			throws ArtifactNotFoundException, ActionExecutionException {
+		return delegateService.artifactActionExec(id, code, params);
+	}
+
+	@Override
+	public <P extends Serializable> Map<String, Object> artifactOnChange(ResourceArtifactType type, String code,
+			P previous, String fieldName, Object fieldValue, Map<String, AnswerValue> answers)
+			throws ArtifactNotFoundException, ResourceFieldNotFoundException, AnswerRequiredException {
+		return delegateService.artifactOnChange(type, code, previous, fieldName, fieldValue, answers);
+	}
+
+	@Override
+	public <P extends Serializable> List<?> artifactReportGenerateData(Long id, String code, P params)
+			throws ArtifactNotFoundException, ReportGenerationException {
+		return delegateService.artifactReportGenerateData(id, code, params);
+	}
 }
--- ripea-ejb/src/main/java/es/caib/ripea/ejb/ExpedientPeticioResourceServiceEjb.java
+++ ripea-ejb/src/main/java/es/caib/ripea/ejb/ExpedientPeticioResourceServiceEjb.java
@@ -1,8 +1,19 @@
 package es.caib.ripea.ejb;
 
 import es.caib.ripea.ejb.base.AbstractServiceEjb;
+import es.caib.ripea.service.intf.base.exception.ActionExecutionException;
+import es.caib.ripea.service.intf.base.exception.AnswerRequiredException;
+import es.caib.ripea.service.intf.base.exception.ArtifactNotFoundException;
+import es.caib.ripea.service.intf.base.exception.ReportGenerationException;
+import es.caib.ripea.service.intf.base.exception.ResourceFieldNotFoundException;
+import es.caib.ripea.service.intf.base.exception.AnswerRequiredException.AnswerValue;
+import es.caib.ripea.service.intf.base.model.ResourceArtifactType;
 import es.caib.ripea.service.intf.resourceservice.ExpedientPeticioResourceService;
 import lombok.experimental.Delegate;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
@@ -17,4 +28,22 @@ public class ExpedientPeticioResourceServiceEjb extends AbstractServiceEjb<Exped
 		this.delegateService = delegateService;
 	}
 	
+	@Override
+	public <P extends Serializable> Serializable artifactActionExec(Long id, String code, P params)
+			throws ArtifactNotFoundException, ActionExecutionException {
+		return delegateService.artifactActionExec(id, code, params);
+	}
+
+	@Override
+	public <P extends Serializable> Map<String, Object> artifactOnChange(ResourceArtifactType type, String code,
+			P previous, String fieldName, Object fieldValue, Map<String, AnswerValue> answers)
+			throws ArtifactNotFoundException, ResourceFieldNotFoundException, AnswerRequiredException {
+		return delegateService.artifactOnChange(type, code, previous, fieldName, fieldValue, answers);
+	}
+
+	@Override
+	public <P extends Serializable> List<?> artifactReportGenerateData(Long id, String code, P params)
+			throws ArtifactNotFoundException, ReportGenerationException {
+		return delegateService.artifactReportGenerateData(id, code, params);
+	}
 }
