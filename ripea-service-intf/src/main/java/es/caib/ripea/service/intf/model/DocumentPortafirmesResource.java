1a9f470caa6ce8d5a7ddfcd4fd3280b0e6d902dc
josepg-limit <josepg@limit.es>
2025-05-20 11:27:13 +0200

    - Action para cancelar una firma

--- ripea-service-intf/src/main/java/es/caib/ripea/service/intf/model/DocumentPortafirmesResource.java
+++ ripea-service-intf/src/main/java/es/caib/ripea/service/intf/model/DocumentPortafirmesResource.java
@@ -1,10 +1,13 @@
 package es.caib.ripea.service.intf.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import org.springframework.data.annotation.Transient;
 
 import es.caib.ripea.service.intf.base.annotation.ResourceConfig;
+import es.caib.ripea.service.intf.base.annotation.ResourceConfigArtifact;
+import es.caib.ripea.service.intf.base.model.ResourceArtifactType;
 import es.caib.ripea.service.intf.dto.MetaDocumentFirmaFluxTipusEnumDto;
 import es.caib.ripea.service.intf.dto.MetaDocumentFirmaSequenciaTipusEnumDto;
 import es.caib.ripea.service.intf.dto.PortafirmesCallbackEstatEnumDto;
@@ -18,10 +21,21 @@ import lombok.Setter;
 @NoArgsConstructor
 @ResourceConfig(
         quickFilterFields = { "nom" },
-        descriptionField = "nom"
+        descriptionField = "nom",
+		artifacts = {
+            @ResourceConfigArtifact(
+                    type = ResourceArtifactType.ACTION,
+                    code = DocumentPortafirmesResource.ACTION_CANCEL_FIRMA,
+                    formClass = Serializable.class,
+                    requiresId = true),
+		}
 )
 public class DocumentPortafirmesResource extends DocumentEnviamentResource {
 
+	private static final long serialVersionUID = -2165194144445671882L;
+
+	public static final String ACTION_CANCEL_FIRMA = "CANCEL_FIRMA";
+	
 	private PortafirmesPrioritatEnumDto prioritat;
 	private Date caducitatData;
 	private String documentTipus;
--- ripea-service/src/main/java/es/caib/ripea/service/resourceservice/DocumentPortafirmesResourceServiceImpl.java
+++ ripea-service/src/main/java/es/caib/ripea/service/resourceservice/DocumentPortafirmesResourceServiceImpl.java
@@ -1,15 +1,27 @@
 package es.caib.ripea.service.resourceservice;
 
+import java.io.Serializable;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import es.caib.ripea.persistence.entity.DocumentEntity;
+import es.caib.ripea.persistence.entity.EntitatEntity;
 import es.caib.ripea.persistence.entity.resourceentity.DocumentPortafirmesResourceEntity;
 import es.caib.ripea.persistence.entity.resourceentity.UsuariResourceEntity;
 import es.caib.ripea.persistence.entity.resourcerepository.UsuariResourceRepository;
 import es.caib.ripea.service.base.service.BaseMutableResourceService;
+import es.caib.ripea.service.firma.DocumentFirmaPortafirmesHelper;
+import es.caib.ripea.service.helper.ConfigHelper;
+import es.caib.ripea.service.helper.DocumentHelper;
+import es.caib.ripea.service.helper.EntityComprovarHelper;
+import es.caib.ripea.service.helper.ExcepcioLogHelper;
 import es.caib.ripea.service.helper.PluginHelper;
+import es.caib.ripea.service.intf.base.exception.ActionExecutionException;
+import es.caib.ripea.service.intf.base.exception.AnswerRequiredException.AnswerValue;
 import es.caib.ripea.service.intf.model.DocumentPortafirmesResource;
 import es.caib.ripea.service.intf.resourceservice.DocumentPortafirmesResourceService;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +34,16 @@ public class DocumentPortafirmesResourceServiceImpl extends BaseMutableResourceS
 
 	private final UsuariResourceRepository usuariResourceRepository;
 	private final PluginHelper pluginHelper;
+	private final ExcepcioLogHelper excepcioLogHelper;
+	private final DocumentHelper documentHelper;
+	private final ConfigHelper configHelper;
+	private final EntityComprovarHelper entityComprovarHelper;
+	private final DocumentFirmaPortafirmesHelper firmaPortafirmesHelper;
 	
     @PostConstruct
-    public void init() {}
+    public void init() {
+    	register(DocumentPortafirmesResource.ACTION_CANCEL_FIRMA, new CancelFirmaActionExecutor());
+    }
     
     @Override
     protected void afterConversion(DocumentPortafirmesResourceEntity entity, DocumentPortafirmesResource resource) {
@@ -34,5 +53,24 @@ public class DocumentPortafirmesResourceServiceImpl extends BaseMutableResourceS
     	} catch (Exception ex) {
     		//No s'ha pogut carregar la URL del fluxe
     	}
+    }
+    
+    private class CancelFirmaActionExecutor implements ActionExecutor<DocumentPortafirmesResourceEntity, Serializable, String> {
+
+		@Override
+		public void onChange(Serializable id, Serializable previous, String fieldName, Object fieldValue, Map<String, AnswerValue> answers, String[] previousFieldNames, Serializable target) {}
+
+		@Override
+		public String exec(String code, DocumentPortafirmesResourceEntity entity, Serializable params) throws ActionExecutionException {
+			try {
+				EntitatEntity entitatEntity = entityComprovarHelper.comprovarEntitat(configHelper.getEntitatActualCodi(), false, false, false, true, false);
+				String rolActual = configHelper.getRolActual();
+				DocumentEntity document = documentHelper.comprovarDocument(entitatEntity.getId(), entity.getDocument().getId(), false, true, false, false, false, rolActual);
+				firmaPortafirmesHelper.portafirmesCancelar(entitatEntity.getId(), document, rolActual);
+			} catch (Exception e) {
+				excepcioLogHelper.addExcepcio("/documentPortafirmes/CancelFirmaActionExecutor", e);
+			}
+			return null;
+		}
     }
 }
