5ae7ddc2a3b893f76b3c8a400c79521a3b0e208c
josepg-limit <josepg@limit.es>
2025-06-19 16:35:24 +0200

    Error al arrancar Springboot

--- ripea-back/src/main/java/es/caib/ripea/back/config/JmsClientConfig.java
+++ ripea-back/src/main/java/es/caib/ripea/back/config/JmsClientConfig.java
@@ -4,6 +4,7 @@ import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWarDeployment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -17,11 +18,13 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsClientConfig {
 	
     @Bean
+    @ConditionalOnWarDeployment
     public ConnectionFactory connectionFactory() throws JMSException{
     	return new ActiveMQConnectionFactory("tcp://localhost:61617");
     }
     
     @Bean
+    @ConditionalOnWarDeployment
     public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
     	return new JmsTemplate(connectionFactory);
     }
