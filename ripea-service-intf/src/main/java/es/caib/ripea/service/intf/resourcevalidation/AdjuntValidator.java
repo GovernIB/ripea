4691ca1c6006c9a4e688007a91f6bce26219e599
Josep Gayà <josepg@limit.es>
2025-07-01 11:34:01 +0200

    #1632
    - Validar tamaño adjunto cambiar traducción

--- ripea-service-intf/src/main/java/es/caib/ripea/service/intf/resourcevalidation/AdjuntValidator.java
+++ ripea-service-intf/src/main/java/es/caib/ripea/service/intf/resourcevalidation/AdjuntValidator.java
@@ -1,19 +1,28 @@
 package es.caib.ripea.service.intf.resourcevalidation;
 
 import es.caib.ripea.service.intf.model.DocumentResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+@Component
+@RequiredArgsConstructor
 public class AdjuntValidator implements ConstraintValidator<AdjuntValid, DocumentResource>{
 
-	@Override
+    private final MessageSource messageSource;
+
+    @Override
 	public boolean isValid(DocumentResource resource, ConstraintValidatorContext context) {
         int maxLength = 52428800; //es.caib.ripea.back.config.WebMvcConfig.MAX_UPLOAD_SIZE;
 
         if (resource.getAdjunt()!=null && (long) maxLength < resource.getAdjunt().getContentLength()) {
+            String message = messageSource.getMessage("es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.adjunt", new Object[]{maxLength}, LocaleContextHolder.getLocale());
             context
-                .buildConstraintViolationWithTemplate("{es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.adjunt}")
+                .buildConstraintViolationWithTemplate(message)
                 .addPropertyNode(DocumentResource.Fields.adjunt)
                 .addConstraintViolation()
                 .disableDefaultConstraintViolation();
@@ -21,8 +30,9 @@ public class AdjuntValidator implements ConstraintValidator<AdjuntValid, Documen
         }
 
         if (resource.getFirmaAdjunt()!=null && (long) maxLength < resource.getFirmaAdjunt().getContentLength()) {
+            String message = messageSource.getMessage("es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.firma", new Object[]{maxLength}, LocaleContextHolder.getLocale());
             context
-                .buildConstraintViolationWithTemplate("{es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.firma}")
+                .buildConstraintViolationWithTemplate(message)
                 .addPropertyNode(DocumentResource.Fields.firmaAdjunt)
                 .addConstraintViolation()
                 .disableDefaultConstraintViolation();
