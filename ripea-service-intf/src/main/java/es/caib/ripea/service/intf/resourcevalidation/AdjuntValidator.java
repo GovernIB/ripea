612e497a68f28db80e62cd6f25b51e2ed823f555
josepg-limit <josepg@limit.es>
2025-07-01 12:00:41 +0200

    - Traduir missatge de tamany maxim de fitxer desde Validador. (Ok springboot) - Poder arrancar amb llibreria commons-fileupload desde Jboss i spring

--- ripea-service-intf/src/main/java/es/caib/ripea/service/intf/resourcevalidation/AdjuntValidator.java
+++ ripea-service-intf/src/main/java/es/caib/ripea/service/intf/resourcevalidation/AdjuntValidator.java
@@ -1,33 +1,51 @@
 package es.caib.ripea.service.intf.resourcevalidation;
-
-import es.caib.ripea.service.intf.model.DocumentResource;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class AdjuntValidator implements ConstraintValidator<AdjuntValid, DocumentResource>{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.env.Environment;
 
+import es.caib.ripea.service.intf.model.DocumentResource;
+
+public class AdjuntValidator implements ConstraintValidator<AdjuntValid, DocumentResource>{
+	
+	@Autowired private Environment springEnvironment;
+	@Autowired private MessageSource messageSource;
+	
 	@Override
 	public boolean isValid(DocumentResource resource, ConstraintValidatorContext context) {
-        int maxLength = 52428800; //es.caib.ripea.back.config.WebMvcConfig.MAX_UPLOAD_SIZE;
+        
+		//Aqui no tenim accés al ConfigHelper, la propietat esta a la taula IPA_CONFIG sense possibilitat de configuració per entitat.
+		int maxLength = Integer.parseInt(springEnvironment.getProperty("es.caib.ripea.maxUploadSize", "52428800"));
 
-        if ((long) maxLength < resource.getAdjunt().getContentLength()) {
-            context
-                .buildConstraintViolationWithTemplate("{es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.adjunt}")
-                .addPropertyNode(DocumentResource.Fields.adjunt)
-                .addConstraintViolation()
-                .disableDefaultConstraintViolation();
-            return false;
-        }
+		String mensaje = messageSource.getMessage(
+			    "es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.adjunt",
+			    new Object[] {(maxLength/1024)/1024},
+			    LocaleContextHolder.getLocale());
+		
+		if (resource.getAdjunt()!=null) {
+	        if ((long) maxLength < resource.getAdjunt().getContentLength()) {
+	            context
+	                .buildConstraintViolationWithTemplate(mensaje)
+	                .addPropertyNode(DocumentResource.Fields.adjunt)
+	                .addConstraintViolation()
+	                .disableDefaultConstraintViolation();
+	            return false;
+	        }
+		}
 
-        if ((long) maxLength < resource.getFirmaAdjunt().getContentLength()) {
-            context
-                .buildConstraintViolationWithTemplate("{es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.firma}")
-                .addPropertyNode(DocumentResource.Fields.firmaAdjunt)
-                .addConstraintViolation()
-                .disableDefaultConstraintViolation();
-            return false;
-        }
+		if (resource.getFirmaAdjunt()!=null) {
+	        if ((long) maxLength < resource.getFirmaAdjunt().getContentLength()) {
+	            context
+	                .buildConstraintViolationWithTemplate(mensaje)
+	                .addPropertyNode(DocumentResource.Fields.firmaAdjunt)
+	                .addConstraintViolation()
+	                .disableDefaultConstraintViolation();
+	            return false;
+	        }
+		}
 
         return true;
 	}
