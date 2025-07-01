eb74dc6e10481423dc1092c5c18f3f5ee906078d
Josep Gayà <josepg@limit.es>
2025-07-01 12:27:48 +0200

    #1632
    - Validar tamaño adjunto

--- ripea-service-intf/src/main/java/es/caib/ripea/service/intf/resourcevalidation/AdjuntValidator.java
+++ ripea-service-intf/src/main/java/es/caib/ripea/service/intf/resourcevalidation/AdjuntValidator.java
@@ -2,49 +2,50 @@ package es.caib.ripea.service.intf.resourcevalidation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.env.Environment;
 
 import es.caib.ripea.service.intf.model.DocumentResource;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class AdjuntValidator implements ConstraintValidator<AdjuntValid, DocumentResource>{
-	
-	@Autowired private Environment springEnvironment;
-	@Autowired private MessageSource messageSource;
-	
+
+	private final Environment springEnvironment;
+	private final MessageSource messageSource;
+
+    public static String formatByteCount(long bytes) {
+        if (bytes < 1024) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(1024));
+        String pre = "KMGTPE".charAt(exp - 1) + "B";
+        return String.format("%.2f %s", bytes / Math.pow(1024, exp), pre);
+    }
+
 	@Override
 	public boolean isValid(DocumentResource resource, ConstraintValidatorContext context) {
-        
-		//Aqui no tenim accés al ConfigHelper, la propietat esta a la taula IPA_CONFIG sense possibilitat de configuració per entitat.
-		int maxLength = Integer.parseInt(springEnvironment.getProperty("es.caib.ripea.maxUploadSize", "52428800"));
+        int maxLength = Integer.parseInt(springEnvironment.getProperty("es.caib.ripea.maxUploadSize", "52428800"));
+        String formatedMaxLength = formatByteCount(maxLength);
+        String message = messageSource.getMessage("es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.adjunt", new Object[]{formatedMaxLength}, LocaleContextHolder.getLocale());
 
-		String mensaje = messageSource.getMessage(
-			    "es.caib.ripea.service.intf.resourcevalidation.AdjuntValid.adjunt",
-			    new Object[] {(maxLength/1024)/1024},
-			    LocaleContextHolder.getLocale());
-		
-		if (resource.getAdjunt()!=null) {
-	        if ((long) maxLength < resource.getAdjunt().getContentLength()) {
-	            context
-	                .buildConstraintViolationWithTemplate(mensaje)
-	                .addPropertyNode(DocumentResource.Fields.adjunt)
-	                .addConstraintViolation()
-	                .disableDefaultConstraintViolation();
-	            return false;
-	        }
+		if (resource.getAdjunt()!=null && (long) maxLength < resource.getAdjunt().getContentLength()) {
+            context
+                .buildConstraintViolationWithTemplate(message)
+                .addPropertyNode(DocumentResource.Fields.adjunt)
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
+            return false;
 		}
 
-		if (resource.getFirmaAdjunt()!=null) {
-	        if ((long) maxLength < resource.getFirmaAdjunt().getContentLength()) {
-	            context
-	                .buildConstraintViolationWithTemplate(mensaje)
-	                .addPropertyNode(DocumentResource.Fields.firmaAdjunt)
-	                .addConstraintViolation()
-	                .disableDefaultConstraintViolation();
-	            return false;
-	        }
+		if (resource.getFirmaAdjunt()!=null && (long) maxLength < resource.getFirmaAdjunt().getContentLength()) {
+            context
+                .buildConstraintViolationWithTemplate(message)
+                .addPropertyNode(DocumentResource.Fields.firmaAdjunt)
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
+            return false;
 		}
 
         return true;
