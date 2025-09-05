22b25a086b4f1c1db17d516d223bb5f4d940b662
Josep Gayà <josepg@limit.es>
2025-09-05 11:35:57 +0200

    #1666
    - Cambiar renderizado de documentos para que sea compatible en firefox

--- ripea-back/src/main/jsapp/ripea-back/src/components/Iframe.tsx
+++ ripea-back/src/main/jsapp/ripea-back/src/components/Iframe.tsx
@@ -1,4 +1,11 @@
-const commonProps = { width: '100%', height: '500px', border: '1px solid lightgray', borderRadius: '4px' }
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+
+// estilos por defecto
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+// const commonProps = { width: '100%', height: '500px', border: '1px solid lightgray', borderRadius: '4px' }
 
 const Iframe = (props:any) => {
     const { src, hidden, style, ...other } = props
@@ -7,6 +14,19 @@ const Iframe = (props:any) => {
         return <></>
     }
 
-    return <iframe src={src} {...other} style={{...commonProps, ...style}}/>
+    // return <iframe src={src} {...other} style={{...commonProps, ...style}}/>
+
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    return (
+        <div style={{ ...style }}>
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                <Viewer
+                    fileUrl={src}   // tu PDF (en public/ o desde una URL)
+                    plugins={[defaultLayoutPluginInstance]}
+                    {...other}
+                />
+            </Worker>
+        </div>
+    );
 }
 export default Iframe;
