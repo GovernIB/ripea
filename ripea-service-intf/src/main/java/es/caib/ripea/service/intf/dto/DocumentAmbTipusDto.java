f566b9f20f5344165e12d9fec9737060f8e71f6a
Josep Gayà <josepg@limit.es>
2025-09-08 11:50:13 +0200

    #1656
    - Revisar DocumentAmbTipusDto

--- ripea-service-intf/src/main/java/es/caib/ripea/service/intf/dto/DocumentAmbTipusDto.java
+++ ripea-service-intf/src/main/java/es/caib/ripea/service/intf/dto/DocumentAmbTipusDto.java
@@ -1,11 +1,13 @@
 package es.caib.ripea.service.intf.dto;
 
 import es.caib.ripea.service.intf.base.model.FileReference;
+import es.caib.ripea.service.intf.base.model.ResourceReference;
+import es.caib.ripea.service.intf.model.MetaDocumentResource;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class DocumentAmbTipusDto {
 	private FileReference fitxer;
-	private Long tipusDocument;
+	private ResourceReference<MetaDocumentResource, Long> tipusDocument;
 }
