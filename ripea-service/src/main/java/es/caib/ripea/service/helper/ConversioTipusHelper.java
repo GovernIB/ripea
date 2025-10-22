09d9f700a53ee248fbc81e02183a08d40bc6c207
Josep Gayà <josepg@limit.es>
2025-10-22 17:01:50 +0200

    Merge branch 'ripea-1.0-dev' into ripea-1.0-wip

--- ripea-service/src/main/java/es/caib/ripea/service/helper/ConversioTipusHelper.java
+++ ripea-service/src/main/java/es/caib/ripea/service/helper/ConversioTipusHelper.java
@@ -854,60 +854,7 @@ public class ConversioTipusHelper {
 						return getInteressatPendentArxiuFromInteressatDto(deproxyInteressatEntity(source));
 					}
 				});
-		
-		mapperFactory.getConverterFactory().registerConverter(
-				new CustomConverter<DocumentNotificacioEntity, DocumentNotificacioDto>() {
 
-					@Override
-					public DocumentNotificacioDto convert(DocumentNotificacioEntity source, Type<? extends DocumentNotificacioDto> destinationType, MappingContext mappingContext) {
-						DocumentNotificacioDto resultat = new DocumentNotificacioDto();
-						resultat.setId(source.getId());
-						resultat.setTipus(source.getTipus());
-						resultat.setDataProgramada(source.getDataProgramada());
-						resultat.setDataCaducitat(source.getDataCaducitat());
-						resultat.setNotificacioIdentificador(source.getNotificacioIdentificador());
-						resultat.setNotificacioEstat(source.getNotificacioEstat());
-						resultat.setServeiTipusEnum(source.getServeiTipusEnum());
-						resultat.setEntregaPostal(source.getEntregaPostal());
-						Set<DocumentEnviamentInteressatDto> enviaments = new HashSet<DocumentEnviamentInteressatDto>();
-						if (source.getDocumentEnviamentInteressats()!=null) {
-							for (DocumentEnviamentInteressatEntity dei: source.getDocumentEnviamentInteressats()) {
-								enviaments.add(convertir(dei, DocumentEnviamentInteressatDto.class));
-							}
-						}
-						resultat.setDocumentEnviamentInteressats(enviaments);
-						resultat.setRegistreData(source.getRegistreData());
-						resultat.setRegistreNumero(source.getRegistreNumero()!=null?Integer.toString(source.getRegistreNumero()):null);
-						resultat.setRegistreNumeroFormatat(source.getRegistreNumeroFormatat());
-						resultat.setAssumpte(source.getAssumpte());
-						DocumentDto doc = new DocumentDto();
-						doc.setId(source.getDocument().getId());
-						doc.setNom(source.getDocument().getNom());
-						doc.setPareId(source.getDocument().getPareId());
-						resultat.setDocument(doc);
-						OrganGestorDto emisor = new OrganGestorDto();
-						emisor.setCodi(source.getEmisor().getCodi());
-						emisor.setNom(source.getEmisor().getNom());
-						resultat.setEmisor(emisor);
-						resultat.setDataEnviada(source.getDataEnviada());
-						resultat.setDataFinalitzada(source.getDataFinalitzada());
-						resultat.setCreatedDate(
-								source.getCreatedDate() != null ?
-										Date.from(source.getCreatedDate().get().atZone(ZoneId.systemDefault()).toInstant()) : null);
-						if (source.getCreatedBy().isPresent()) {
-			      			UsuariEntity ue = usuariRepository.findByCodi(source.getCreatedBy().get());
-			      			UsuariDto uDto = new UsuariDto();
-			      			uDto.setCodi(ue.getCodi());
-			      			uDto.setNom(ue.getNom());
-			      			uDto.setNif(ue.getNif());
-			      			uDto.setEmail(ue.getEmail());
-			      			resultat.setCreatedBy(uDto);
-						}						
-						resultat.setDataEnviada(source.getDataEnviada());
-						return resultat;
-					}
-				});
-		
 		mapperFactory.getConverterFactory().registerConverter(
 				new CustomConverter<ExecucioMassivaEntity, ExecucioMassivaDto>() {
 					@Override
