b0d9e399dee617e3d9f2ea32c4e9026c0de94ced
Josep Gayà <josepg@limit.es>
2025-11-27 15:22:19 +0100

    Correcciones de la versión 1.0.3
    - Revisar estilos de estructura en arbol

--- ripea-back/src/main/jsapp/ripea-back/src/components/StyledMuiGrid.tsx
+++ ripea-back/src/main/jsapp/ripea-back/src/components/StyledMuiGrid.tsx
@@ -169,10 +169,6 @@ const StyledMuiGrid = (props:StyledMuiGridProps) => {
                         text-overflow: ellipsis !important;
                     }
                     
-                    .css-d0uhtl {
-                        margin-left: 45px;
-                    }
-                    
                     .MuiDataGrid-checkboxInput {
                         transform: scale(0.8);
                     }
