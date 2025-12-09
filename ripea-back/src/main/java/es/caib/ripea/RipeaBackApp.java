febdda539213337264ff1dd19674b2de74490c1b
Josep Gayà <josepg@limit.es>
2025-12-09 11:12:55 +0100

    Revisio de error visibilitat entre moduls i dependencies maven

--- ripea-back/src/main/java/es/caib/ripea/RipeaBackApp.java
+++ ripea-back/src/main/java/es/caib/ripea/RipeaBackApp.java
@@ -48,13 +48,13 @@ import java.util.jar.Manifest;
 })
 @ComponentScan(
 		basePackages = { BaseConfig.BASE_PACKAGE },
-		excludeFilters = {
+		useDefaultFilters = false,
+		includeFilters = {
 				@ComponentScan.Filter(
 						type = FilterType.REGEX,
 						pattern = {
-								"es\\.caib\\." + BaseConfig.APP_NAME + "\\.service\\..*",
-								"es\\.caib\\." + BaseConfig.APP_NAME + "\\.persistence\\..*",
-								"es\\.caib\\." + BaseConfig.APP_NAME + "\\.ejb\\..*" })
+								"es\\.caib\\." + BaseConfig.APP_NAME + "\\.back\\..*",
+								"es\\.caib\\." + BaseConfig.APP_NAME + "\\.service.intf.base.util\\..*" })
 		})
 @PropertySource(
 		ignoreResourceNotFound = true,
